4a2d090043cecb32d4804b4ebabab9f3439f7456
Aiswaryapc <aiswaryapchandran3@gmail.com>
2022-01-19 12:27:11 +0530

    first

--- lib/screens/main/components/side_menu.dart
+++ lib/screens/main/components/side_menu.dart
@@ -1,85 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class SideMenu extends StatelessWidget {
-  const SideMenu({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            child: Image.asset("assets/images/logo.png"),
-          ),
-          DrawerListTile(
-            title: "Dashboard",
-            svgSrc: "assets/icons/menu_dashbord.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Analytics",
-            svgSrc: "assets/icons/menu_tran.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Database",
-            svgSrc: "assets/icons/menu_task.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Reports",
-            svgSrc: "assets/icons/menu_doc.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Approvals",
-            svgSrc: "assets/icons/menu_store.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Access Rights",
-            svgSrc: "assets/icons/menu_store.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Recommendations",
-            svgSrc: "assets/icons/menu_store.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Sales / Marketing",
-            svgSrc: "assets/icons/menu_store.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "AI",
-            svgSrc: "assets/icons/menu_store.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Notification",
-            svgSrc: "assets/icons/menu_notification.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Profile",
-            svgSrc: "assets/icons/menu_profile.svg",
-            press: () {},
-          ),
-          DrawerListTile(
-            title: "Settings",
-            svgSrc: "assets/icons/menu_setting.svg",
-            press: () {},
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class DrawerListTile extends StatelessWidget {
   const DrawerListTile({
     Key? key,
@@ -94,17 +15,19 @@ class DrawerListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: press,
-      horizontalTitleGap: 0.0,
-      leading: SvgPicture.asset(
-        svgSrc,
-        color: Colors.white54,
-        height: 16,
-      ),
-      title: Text(
-        title,
-        style: TextStyle(color: Colors.white54),
+    return InkWell(
+      child: ListTile(
+        onTap: press,
+        horizontalTitleGap: 0.0,
+        leading: SvgPicture.asset(
+          svgSrc,
+          color: Colors.white54,
+          height: 16,
+        ),
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white54),
+        ),
       ),
     );
   }
--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -2,16 +2,25 @@ import 'package:admin/controllers/MenuController.dart';
 import 'package:admin/responsive.dart';
 import 'package:admin/screens/dashboard/dashboard_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
+import '../analytics/analytics_main.dart';
 import 'components/side_menu.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int title=1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       key: context.read<MenuController>().scaffoldKey,
-      drawer: SideMenu(),
+      drawer: sideMenu(),
       body: SafeArea(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,16 +30,120 @@ class MainScreen extends StatelessWidget {
               Expanded(
                 // default flex = 1
                 // and it takes 1/6 part of the screen
-                child: SideMenu(),
+                child: sideMenu(),
               ),
-            Expanded(
+            if(title==1) Expanded(
               // It takes 5/6 part of the screen
               flex: 5,
               child: DashboardScreen(),
             ),
+            if(title==2)
+              Expanded(
+                // It takes 5/6 part of the screen
+                flex: 5,
+                child: AnalyticsMainScreen(),
+              ),
           ],
         ),
       ),
     );
   }
+
+Widget sideMenu(){
+    return  Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            child: Image.asset("assets/images/logo.png"),
+          ),
+          DrawerListTile(
+            title: "Dashboard",
+            svgSrc: "assets/icons/menu_dashbord.svg",
+            press: () {
+              setState((){
+                title=1;
+              });
+            },
+          ),
+          DrawerListTile(
+            title: "Analytics",
+            svgSrc: "assets/icons/menu_tran.svg",
+            press: () {setState((){
+              title=2;
+            });},
+          ),
+          DrawerListTile(
+            title: "Database",
+            svgSrc: "assets/icons/menu_task.svg",
+            press: () {setState((){
+              title=3;
+            });},
+          ),
+          DrawerListTile(
+            title: "Reports",
+            svgSrc: "assets/icons/menu_doc.svg",
+            press: () {setState((){
+              title=4;
+            });},
+          ),
+          DrawerListTile(
+            title: "Approvals",
+            svgSrc: "assets/icons/menu_store.svg",
+            press: () {setState((){
+              title=5;
+            });},
+          ),
+          DrawerListTile(
+            title: "Access Rights",
+            svgSrc: "assets/icons/menu_store.svg",
+            press: () {setState((){
+              title=6;
+            });},
+          ),
+          DrawerListTile(
+            title: "Recommendations",
+            svgSrc: "assets/icons/menu_store.svg",
+            press: () {setState((){
+              title=7;
+            });},
+          ),
+          DrawerListTile(
+            title: "Sales / Marketing",
+            svgSrc: "assets/icons/menu_store.svg",
+            press: () {setState((){
+              title=8;
+            });},
+          ),
+          DrawerListTile(
+            title: "AI",
+            svgSrc: "assets/icons/menu_store.svg",
+            press: () {setState((){
+              title=9;
+            });},
+          ),
+          DrawerListTile(
+            title: "Notification",
+            svgSrc: "assets/icons/menu_notification.svg",
+            press: () {setState((){
+              title=10;
+            });},
+          ),
+          DrawerListTile(
+            title: "Profile",
+            svgSrc: "assets/icons/menu_profile.svg",
+            press: () {setState((){
+              title=11;
+            });},
+          ),
+          DrawerListTile(
+            title: "Settings",
+            svgSrc: "assets/icons/menu_setting.svg",
+            press: () {setState((){
+              title=12;
+            });},
+          ),
+        ],
+      ),
+    );
+}
 }
