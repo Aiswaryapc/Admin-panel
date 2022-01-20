6cda84d109b105c7a229ad16369208a9bd4ca5dd
Aiswaryapc <aiswaryapchandran3@gmail.com>
2022-01-20 12:43:32 +0530

    Analytics

---
+++ lib/models/preferences_data.dart
@@ -0,0 +1,49 @@
+class PreferencesFile {
+  final String? icon, title, loc ;
+
+  PreferencesFile( {this.icon, this.title, this.loc});
+}
+List demoPreferencesFiles = [
+  PreferencesFile(
+    icon: "assets/icons/xd_file.svg",
+    title: "Shop 1",
+    loc: "India",
+
+  ),
+  PreferencesFile(
+    icon: "assets/icons/Figma_file.svg",
+    title: "Shop 2",
+    loc: "China",
+
+  ),
+  PreferencesFile(
+    icon: "assets/icons/doc_file.svg",
+    title: "Shop 3",
+    loc: "India",
+
+  ),
+  PreferencesFile(
+    icon: "assets/icons/sound_file.svg",
+    title: "Shop 4",
+    loc: "Pakistan",
+
+  ),
+  PreferencesFile(
+    icon: "assets/icons/media_file.svg",
+    title: "Shop 5",
+    loc: "China",
+
+  ),
+  PreferencesFile(
+    icon: "assets/icons/pdf_file.svg",
+    title: "Shop 6",
+    loc: "Japan",
+
+  ),
+  PreferencesFile(
+    icon: "assets/icons/excle_file.svg",
+    title: "Shop 7",
+    loc: "India",
+
+  ),
+];
--- lib/screens/analytics/analytics_main.dart
+++ lib/screens/analytics/analytics_main.dart
@@ -1,10 +1,62 @@
+import 'package:admin/screens/analytics/preference.dart';
 import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+import '../../responsive.dart';
+import '../dashboard/components/my_fields.dart';
+import '../dashboard/components/recent_files.dart';
+import 'behavioural_pattern.dart';
 
 class AnalyticsMainScreen extends StatelessWidget {
   const AnalyticsMainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final Size _size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: defaultPadding),
+            Text(
+              "Analytics",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(height: defaultPadding),
+            Text(
+              "Behavioural Pattern of Users",
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            SizedBox(height: defaultPadding),
+            Row(
+              children: [
+                Expanded(flex: 5,
+                  child: Responsive(
+                    mobile: BehaviouralCardGridView (
+                      crossAxisCount: _size.width < 650 ? 2 : 4,
+                      childAspectRatio: _size.width < 650 ? 1.3 : 1,
+                    ),
+                    tablet: BehaviouralCardGridView (),
+                    desktop: BehaviouralCardGridView (
+                      childAspectRatio: _size.width < 1400 ? 1.1 : 1.4,
+                    ),
+                  ),
+                ),
+                SizedBox(height: defaultPadding),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Expanded(flex: 3,
+                      child: Preferences()),
+                ),
+              ],
+            ),
+
+            SizedBox(height: defaultPadding),
+          ],
+        ),
+      ),
+    );
   }
 }
