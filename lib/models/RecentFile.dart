a50afd2a9a641a0b1eb5554d152c15b219f5776b
Aiswarya p chandran <55802944+Aiswaryapc@users.noreply.github.com>
2022-01-21 18:27:40 +0530

    Revert "Web screens"

--- lib/models/RecentFile.dart
+++ lib/models/RecentFile.dart
@@ -1,14 +1,8 @@
 class RecentFile {
   final String? icon, title, loc, followers;
 
-  RecentFile({
-    this.icon,
-    this.title,
-    this.loc,
-    this.followers,
-  });
+  RecentFile({this.icon, this.title, this.loc, this.followers});
 }
-
 List demoRecentFiles = [
   RecentFile(
     icon: "assets/icons/xd_file.svg",
@@ -19,7 +13,7 @@ List demoRecentFiles = [
   RecentFile(
     icon: "assets/icons/Figma_file.svg",
     title: "Shop 2",
-    loc: "Mumbai",
+    loc: "China",
     followers: "21m",
   ),
   RecentFile(
@@ -31,19 +25,19 @@ List demoRecentFiles = [
   RecentFile(
     icon: "assets/icons/sound_file.svg",
     title: "Shop 4",
-    loc: "Tamil Nadu",
+    loc: "Pakistan",
     followers: "5.5m",
   ),
   RecentFile(
     icon: "assets/icons/media_file.svg",
     title: "Shop 5",
-    loc: "Kerala",
+    loc: "China",
     followers: "1.3m",
   ),
   RecentFile(
     icon: "assets/icons/pdf_file.svg",
     title: "Shop 6",
-    loc: "Uttar Pradesh",
+    loc: "Japan",
     followers: "5k",
   ),
   RecentFile(
@@ -51,64 +45,5 @@ List demoRecentFiles = [
     title: "Shop 7",
     loc: "India",
     followers: "2k",
-  ),
-];
-
-class ReportFile {
-  final String? icon, title, loc, followers, numOfVideo;
-
-  ReportFile(
-      {this.icon, this.title, this.loc, this.followers, this.numOfVideo});
-}
-
-List demoReportFiles = [
-  ReportFile(
-    icon: "assets/icons/xd_file.svg",
-    title: "Shop 1",
-    loc: "India",
-    followers: "33m",
-    numOfVideo: "230",
-  ),
-  ReportFile(
-    icon: "assets/icons/Figma_file.svg",
-    title: "Shop 2",
-    loc: "Mumbai",
-    followers: "21m",
-    numOfVideo: "1000",
-  ),
-  ReportFile(
-    icon: "assets/icons/doc_file.svg",
-    title: "Shop 3",
-    loc: "India",
-    followers: "13m",
-    numOfVideo: "200",
-  ),
-  ReportFile(
-    icon: "assets/icons/sound_file.svg",
-    title: "Shop 4",
-    loc: "Tamil Nadu",
-    followers: "5.5m",
-    numOfVideo: "1390",
-  ),
-  ReportFile(
-    icon: "assets/icons/media_file.svg",
-    title: "Shop 5",
-    loc: "Kerala",
-    followers: "1.3m",
-    numOfVideo: "730",
-  ),
-  ReportFile(
-    icon: "assets/icons/pdf_file.svg",
-    title: "Shop 6",
-    loc: "Uttar Pradesh",
-    followers: "5k",
-    numOfVideo: "840",
-  ),
-  ReportFile(
-    icon: "assets/icons/excle_file.svg",
-    title: "Shop 7",
-    loc: "India",
-    followers: "2k",
-    numOfVideo: "490",
   ),
 ];
